3521fd791920492c26b00bc9cc4a94c35fe82ab5
romainD <romain.deschamps61@sfr.fr>
2023-11-16 23:34:46 +0100

    NameUpdate2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using EnableFeatProgressInCustomModeLegacy;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("EnableFeatProgressInCustomMode")]
+[assembly: AssemblyTitle("EnableFeatProgressInCustomModeLegacy")]
 [assembly: AssemblyCopyright("MIT Licence")]
 
 [assembly: AssemblyVersion("1.3.0")]
 [assembly: AssemblyFileVersion("1.3.0")]
-[assembly: MelonInfo(typeof(EnableFeatProgressInCustomModeLegacy.Mod), "EnableFeatProgressInCustomModeLegacy", "1.3.0", "zeobviouslyfakeacc, Romain")]
+[assembly: MelonInfo(typeof(EnableFeatProgressInCustomModeLegacy.Mod), "EnableFeatProgressInCustomModeLegacy", "1.3.0", "zeobviouslyfakeacc")]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
